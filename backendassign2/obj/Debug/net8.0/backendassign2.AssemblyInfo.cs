3c0f739c1c60dbe60d6dac5f09be1c0cc1c1bc25
AsgerLeth <202004818@post.au.dk>
2024-11-04 11:01:56 +0100

    New gitignore

--- backendassign2/obj/Debug/net8.0/backendassign2.AssemblyInfo.cs
+++ backendassign2/obj/Debug/net8.0/backendassign2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backendassign2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cde452852d97c27f0ead57f9d90ec4f4e4efdbc1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b127cdaabd46360421d3c2e82b21c261a56057a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("backendassign2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backendassign2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
